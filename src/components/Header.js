cd4984b8faa62dcb217ad6f1357ee6164368915f
Roman Ismagilov <roman.j123@ya.ru>
2021-03-09 23:30:46 +0300

    fix блок навигации при логине

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,30 +27,32 @@ function Header({loggedIn, loggedOut, email}) {
             <span></span>
         </button>
         }
-
-      <ul className={`header__login ${loggedIn ? 'header__login_column' : ''} ${!openNavigation ? '' : 'header__login_column-on'}`}>
-        <Switch>
-            <Route path="/sign-in">
-              <li className="header__login-item">
-                <Link className="header__login-link" to="/sign-up">Регистрация</Link>
-              </li>
-            </Route>
-            <Route path="/sign-up">
-              <li className="header__login-item">
-                <Link className="header__login-link" to="/sign-in">Войти</Link>
-              </li>
-            </Route>
-          {loggedIn ?
-            <>
-              <li className="header__login-item header__login-item_type_email">{email}</li>
-              <li className="header__login-item">
-                <span className="header__login-link header__login-link_type_logout" onClick={loggedOut}>Выйти</span>
-              </li>
-            </>:
-            ''
-          }
-        </Switch>
-      </ul>
+        <ul className={`header__login ${!loggedIn ? 
+          'header__login_on' 
+          : `${!openNavigation ? '' : 'header__login_column header__login_column-off'}`}`}
+        >
+          <Switch>
+              <Route path="/sign-in">
+                <li className="header__login-item">
+                  <Link className="header__login-link" to="/sign-up">Регистрация</Link>
+                </li>
+              </Route>
+              <Route path="/sign-up">
+                <li className="header__login-item">
+                  <Link className="header__login-link" to="/sign-in">Войти</Link>
+                </li>
+              </Route>
+            {loggedIn ?
+              <>
+                <li className="header__login-item header__login-item_type_email">{email}</li>
+                <li className="header__login-item">
+                  <span className="header__login-link header__login-link_type_logout" onClick={loggedOut}>Выйти</span>
+                </li>
+              </>:
+              ''
+            }
+          </Switch>
+        </ul>
       </div>
     </header>
     )
